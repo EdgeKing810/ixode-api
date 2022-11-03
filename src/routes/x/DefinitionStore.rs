a10f7624984a5820c2057ec1b4393a5d75765b6f
Kishan Takoordyal <kishan@konnect.dev>
2022-11-03 19:34:57 +0400

    x: finish impl OBJECT

--- src/routes/x/DefinitionStore.rs
+++ src/routes/x/DefinitionStore.rs
@@ -16,7 +16,7 @@ use crate::components::routing::submodules::sub_next_condition_type::NextConditi
 use crate::components::routing::submodules::sub_operation::Operation;
 use crate::components::routing::submodules::sub_operation_type::OperationType;
 use crate::components::routing::submodules::sub_ref_data::RefData;
-use crate::data_converter::{convert_to_raw, RawPair};
+use crate::data_converter::{convert_to_raw, CustomStructurePair, RawPair, StructurePair};
 use crate::routes::x::LocalParamData;
 use crate::utils::{auto_fetch_all_collections, auto_fetch_all_data, auto_fetch_all_mappings};
 
@@ -1051,6 +1051,125 @@ impl DefinitionStore {
                 ref_name: function_block.local_name,
                 index: index,
                 data: final_data,
+            };
+        } else if block_name == "OBJECT" {
+            let object_block = current_route.flow.objects[index].clone();
+
+            let mut structures = Vec::<StructurePair>::new();
+            let mut custom_structures = Vec::<CustomStructurePair>::new();
+
+            for pair in object_block.pairs {
+                let current_data = match resolve_ref_data(
+                    &pair.data,
+                    global_blocks,
+                    all_definitions,
+                    current_index,
+                ) {
+                    Ok(d) => d,
+                    Err(e) => {
+                        return Err(e);
+                    }
+                };
+
+                let mut processed_data = String::new();
+
+                match current_data {
+                    DefinitionData::STRING(s) => {
+                        processed_data = s;
+                    }
+                    DefinitionData::INTEGER(i) => {
+                        processed_data = i.to_string();
+                    }
+                    DefinitionData::FLOAT(f) => {
+                        processed_data = f.to_string();
+                    }
+                    DefinitionData::BOOLEAN(b) => {
+                        processed_data = b.to_string();
+                    }
+                    DefinitionData::ARRAY(a) => {
+                        for (i, item) in a.iter().enumerate() {
+                            match item {
+                                DefinitionData::STRING(s) => {
+                                    processed_data = format!(
+                                        "{}{}{}",
+                                        processed_data,
+                                        if i == 0 { "" } else { "," },
+                                        s
+                                    );
+                                }
+                                DefinitionData::INTEGER(int) => {
+                                    processed_data = format!(
+                                        "{}{}{}",
+                                        processed_data,
+                                        if i == 0 { "" } else { "," },
+                                        int
+                                    );
+                                }
+                                DefinitionData::FLOAT(f) => {
+                                    processed_data = format!(
+                                        "{}{}{}",
+                                        processed_data,
+                                        if i == 0 { "" } else { "," },
+                                        f
+                                    );
+                                }
+                                DefinitionData::BOOLEAN(b) => {
+                                    processed_data = format!(
+                                        "{}{}{}",
+                                        processed_data,
+                                        if i == 0 { "" } else { "," },
+                                        b
+                                    );
+                                }
+                                _ => {}
+                            }
+                        }
+                    }
+                    DefinitionData::DATA(d) => {
+                        let mut current_structures = Vec::<StructurePair>::new();
+
+                        for structure in d.structures {
+                            current_structures.push(StructurePair {
+                                id: structure.id,
+                                value: structure.value,
+                            });
+                        }
+
+                        for custom_structure in d.custom_structures {
+                            for structure in custom_structure.structures {
+                                current_structures.push(StructurePair {
+                                    id: format!("{}.{}", custom_structure.id, structure.id),
+                                    value: structure.value,
+                                });
+                            }
+                        }
+
+                        custom_structures.push(CustomStructurePair {
+                            id: pair.id,
+                            structures: current_structures,
+                        });
+
+                        continue;
+                    }
+                    DefinitionData::NULL => {}
+                    DefinitionData::UNDEFINED => {}
+                }
+
+                structures.push(StructurePair {
+                    id: pair.id,
+                    value: processed_data,
+                });
+            }
+
+            actual_definition = DefinitionStore {
+                block_name: block_name.to_string(),
+                ref_name: object_block.local_name,
+                index: index,
+                data: DefinitionData::DATA(RawPair {
+                    structures: structures,
+                    custom_structures: custom_structures,
+                    published: false,
+                }),
             };
         }
 
